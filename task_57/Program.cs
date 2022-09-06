0ac4ba37f0c1ffad19bd63c2ca7b8716fb7fb95a
Andrew Glinsky <glinsky_andrew@hotmail.com>
2022-09-06 19:36:19 +0300

    modified the code

--- task_57/Program.cs
+++ task_57/Program.cs
@@ -1,11 +1,13 @@
-﻿/*Задача 57: Составить частотный словарь элементов
-двумерного массива. Частотный словарь содержит
-информацию о том, сколько раз встречается элемент
-входных данных.*/
+﻿/* Задача 57:
+ * Составить частотный словарь элементов
+ * двумерного массива. Частотный словарь содержит
+ * информацию о том, сколько раз встречается элемент
+ * входных данных.
+ */
 
 Console.Clear();
 
-int[,] GetMatrix(int m, int n)
+int[,] GetMatrix(int m, int n) // Создание рандомого массива, заполненного случайными числами
 {
     int[,] matrix = new int[m, n];
     Random rnd = new Random();
@@ -17,9 +19,9 @@ int[,] GetMatrix(int m, int n)
         }
     }
     return matrix;
-} // Создание рандомого массива, заполненного случайными числами
+} 
 
-void PrintMatrix(int[,] array)
+void PrintMatrix(int[,] array) // Печать двумерного массива
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
@@ -31,9 +33,9 @@ void PrintMatrix(int[,] array)
         }
         Console.WriteLine();
     }
-} // Печать двумерного массива
+} 
 
-int[] СonversionArray(int[,] array) // Переводим двумерный массив в одномерный
+int[] ConversionArray(int[,] array) // Переводим двумерный массив в одномерный
 {
     int[] arrayNew = new int[array.Length];
     int m = 0;
@@ -49,46 +51,6 @@ int[] СonversionArray(int[,] array) // Переводим двумерный м
     return arrayNew;
 }
 
-
-int[] array(int[,] array)
-{
-    int[] arrayNew = new int[10];
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            arrayNew[array[i, j]]++;
-        }
-    }
-    return arrayNew;
-}
-
-//int[] array2(int[,] array)
-{
-    int[] arrayNew = new int[10];
-    int m = 0;
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            arrayNew[m] = array[i, j];
-            m++;
-        }
-    }
-    Array.Sort(arrayNew);
-    return arrayNew;
-}
-
-void PrintArray(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (i == 0) Console.Write("[");
-        if (i < array.Length - 1) Console.Write(array[i] + ",");
-        else Console.Write(array[i] + "]");
-    }
-}
-
 void WriteDigitCountToConsole(int[] arrayNew)
 {
     int number = arrayNew[0];
@@ -98,21 +60,20 @@ void WriteDigitCountToConsole(int[] arrayNew)
         if (arrayNew[i] == number) count++;
         else
         {
-            Console.WriteLine($"Число {number} -> {count} раз");
+            Console.WriteLine($"Число {number} -> {count} раз(-а)");
             number = arrayNew[i];
             count = 1;
         }
         if (i == arrayNew.Length - 1)
-            Console.WriteLine($"Число {number} -> {count} раз");
+            Console.WriteLine($"Число {number} -> {count} раз(-а)");
     }
 }
 
-
-
 int[,] matrixResult = GetMatrix(3, 3);
 PrintMatrix(matrixResult);
-
-
-
+Console.WriteLine();
 int[] arrayNew = ConversionArray(matrixResult);
-WriteDigitCountConsole(arrayNew);
+WriteDigitCountToConsole(arrayNew);
+Console.WriteLine();
+
+
