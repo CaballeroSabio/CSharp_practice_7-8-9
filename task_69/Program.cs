1e84729a1b3f8db7e8a66fe84603b1da18c94203
Andrew Glinsky <glinsky_andrew@hotmail.com>
2022-09-09 19:07:50 +0300

    updated the code

--- task_69/Program.cs
+++ task_69/Program.cs
@@ -1,25 +1,30 @@
-﻿/*Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
-возводит число А в целую степень B с помощью рекурсии.
-A = 3; B = 5 -> 243 (3⁵)
-A = 2; B = 3 -> 8*/
+﻿/* Задача 69:
+ * Напишите программу, которая на вход принимает два числа A и B,
+ * и возводит число А в целую степень B с помощью рекурсии.
+ * A = 3; B = 5 -> 243 (3⁵)
+ * A = 2; B = 3 -> 8
+ */
 
+Console.Clear();
 
-int GetPower(int num, int num2)
-{
-    if (num2 == 0) return 1;
-    return GetPower(num, num2 - 1) * num;
-}
-if (number2 >= 0)
-{
-    int result = GetPower(number, number2);
-    Console.WriteLine(result);
-}
-else Console.WriteLine("Степень должна быть натуральна");
-Console.Write("Введите первое число : ");
-int number = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число : ");
+Console.Write("Задайте первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Задайте второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
+int GetPower(int num1, int num2)
+{
+    if (num2 == 0) return 1;
+    return GetPower (num1, num2 - 1) * num1;
+}
+
+if (number2 >= 0)
+{
+    int result = GetPower(number1, number2);
+    Console.WriteLine(result);
+}
+else Console.WriteLine("Степень должна быть натуральной");
 
 
 
