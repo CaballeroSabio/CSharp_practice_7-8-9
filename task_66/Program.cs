37e8294ec6ba60df23dbd4c680cce0d9af243aad
Andrew Glinsky <glinsky_andrew@hotmail.com>
2022-09-09 19:06:11 +0300

    created the new code

---
+++ task_66/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Задача 66:
+ * Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+ * M = 1; N = 15 -> 120
+ * M = 4; N = 8. -> 30
+ */
+
+Console.Clear();
+
+Console.WriteLine("Введите начальное число диапазона сложения:");
+int m = Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine("Введите конечное число диапазона сложения:");
+int n = Convert.ToInt32 (Console.ReadLine());
+
+void GetSumDigitForScale (int num1, int num2, int sum)
+{
+    if (num1 >= 0 && num2 >= 0)
+    {
+        if (num1 > num2) 
+            {
+                Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}");
+                return;            
+            }
+            sum += (num1++);
+            GetSumDigitForScale(num1, num2, sum);
+    }
+    else
+    {
+        Console.WriteLine("Введены отрицательные числа");
+        return;
+    }    
+}
+
+GetSumDigitForScale (m, n, 0);
+
